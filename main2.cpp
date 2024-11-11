819000b223f3f45f7b242b4f1137faae920dc5b8
sgaeteBSD <419097@bsd48.org>
2024-11-10 18:28:04 -0800

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,3 +1,52 @@
+#include "Media.h"
+#include "Game.h"
+#include "Movie.h"
+#include "Music.h"
+#include <vector>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+void add(vector<Media*>& med);
+void search(vector<Media*>& med);
+//void deleter(vector<Media*>& med);
+//void quit(vector<Media*>& med, bool& input);
+
+int main() {
+    vector<Media*> med;
+
+    bool input = true;
+    while (input == true) {
+        cout << "Input a command." << endl;
+        char command[6] = "";
+
+        cin >> command;
+        cin.ignore();
+
+        if (strcmp(command, "ADD") == 0) {
+            add(med);
+            input = true; //run command loop again
+        }
+        else if (strcmp(command, "SEARCH") == 0) {
+            search(med);
+            input = true;
+        }
+        else if (strcmp(command, "DELETE") == 0) {
+            //deleter(med);
+            input = true;
+        }
+        else if (strcmp(command, "QUIT") == 0) {
+            //quit(med, input);
+        }
+        else { //if command isn't 1 of the 4
+            cout << "Invalid input! Try again." << endl;
+        }
+    }
+    cout << "Goodbye!" << endl;
+    return 0;
+}
+
 void add(vector<Media*>& med) {
     cout << "GAME. MOVIE, or MUSIC?" << endl;
     char medType[5] = "";
@@ -29,7 +78,60 @@ void add(vector<Media*>& med) {
             cout << "Failed to create Game object!" << endl;
         }
     }
-    // Repeat for MOVIE and MUSIC as before...
+    else if (strcmp(medType, "MOVIE") == 0) {
+        char tempDirector[80];
+        int tempDur;
+        int tempRat;
+        cout << "Title?" << endl;
+        cin.getline(tempTitle, 80, '\n');
+        cout << "Year?" << endl;
+        cin >> tempYear;
+        cin.ignore();
+        cout << "Director?" << endl;
+        cin.getline(tempDirector, 80, '\n');
+        cout << "Duration (minutes)?" << endl;
+        cin >> tempDur;
+        cin.ignore();
+        cout << "Rating? (out of 10)" << endl;
+        cin >> tempRat;
+        cin.ignore();
+
+        Movie* nMovie = new Movie(tempTitle, tempYear, tempDirector, tempDur, tempRat);
+        if (nMovie != nullptr) {
+            med.push_back(nMovie);
+            cout << "Pointer to Movie pushed to med, address: " << nMovie << endl;
+        } else {
+            cout << "Failed to create Movie object!" << endl;
+        }
+    }
+    else if (strcmp(medType, "MUSIC") == 0) {
+        char tempArtist[80];
+        char tempPub[80];
+        int tempDur;
+        cout << "Title?" << endl;
+        cin.getline(tempTitle, 80, '\n');
+        cout << "Year?" << endl;
+        cin >> tempYear;
+        cin.ignore();
+        cout << "Artist?" << endl;
+        cin.getline(tempArtist, 80, '\n');
+        cout << "Publisher?" << endl;
+        cin.getline(tempPub, 80, '\n');
+        cout << "Duration (minutes)?" << endl;
+        cin >> tempDur;
+        cin.ignore();
+
+        Music* nMusic = new Music(tempTitle, tempYear, tempArtist, tempPub, tempDur);
+        if (nMusic != nullptr) {
+            med.push_back(nMusic);
+            cout << "Pointer to Music pushed to med, address: " << nMusic << endl;
+        } else {
+            cout << "Failed to create Music object!" << endl;
+        }
+    }
+    else {
+        cout << "Invalid media type!" << endl;
+    }
 }
 
 void search(vector<Media*>& med) {
